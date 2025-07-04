34421ac27b2d2c4aa1521d1375d27017ed2daaa3
Rocky Haque <rockyhaque99@gmail.com>
2025-07-04 23:38:48 +0600

    fix: server cover img rouned

--- recyclo-client/src/pages/Shared/Cover/Cover.jsx
+++ recyclo-client/src/pages/Shared/Cover/Cover.jsx
@@ -2,29 +2,28 @@ import { Parallax } from "react-parallax";
 
 const Cover = ({ img, title }) => {
   return (
-    <Parallax
-      blur={{ min: -50, max: 50 }}
-      bgImage={img}
-      bgImageAlt="the service"
-      strength={-200}
-    >
-      <div
-        className="hero h-[400px]"
-        
+    <div className="rounded-xl overflow-hidden">
+      <Parallax
+        blur={{ min: -50, max: 50 }}
+        bgImage={img}
+        bgImageAlt="the service"
+        strength={-200}
       >
-        <div className="hero-overlay bg-opacity-60"></div>
-        <div className="hero-content text-center text-neutral-content">
-          <div className="max-w-md">
-            <h1 className="mb-5 text-5xl font-bold uppercase">{title}</h1>
-            <p className="mb-5">
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
-              et a id nisi.
-            </p>
+        <div className="hero h-[400px]">
+          <div className="hero-overlay bg-opacity-60"></div>
+          <div className="hero-content text-center text-neutral-content">
+            <div className="max-w-lg">
+              <h1 className="mb-5 text-5xl font-bold uppercase">{title}</h1>
+              <p className="mb-5">
+                We provide eco-friendly recycling solutions for paper, plastic,
+                metal, textiles, electronics, and glass - helping communities
+                reduce waste and promote sustainability.
+              </p>
+            </div>
           </div>
         </div>
-      </div>
-    </Parallax>
+      </Parallax>
+    </div>
   );
 };
 
